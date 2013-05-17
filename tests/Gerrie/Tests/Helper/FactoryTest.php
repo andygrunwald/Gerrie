aa1c3d49106aae07d329f73189ee85721497c19b
Andy Grunwald <andygrunwald@gmail.com>
2013-05-17 20:08:14 +0200

    Added first unit test files

---
+++ tests/Gerrie/Tests/Helper/FactoryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Gerrie\Tests\Helper;
+
+
+class FactoryTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * @test
+     */
+    public function bootstrapSSHDataService() {
+        $this->markTestIncomplete('This test has not been implemented yet.');
+    }
+}
