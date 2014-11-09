858d8f1bca5056703cf7cd78efd1e1bb54fc707b
Andy Grunwald <andygrunwald@gmail.com>
2014-11-09 13:22:53 +0100

    Added DataServiceInterface to enable a unique interface

--- src/Gerrie/Component/DataService/DataServiceInterface.php
+++ src/Gerrie/Component/DataService/DataServiceInterface.php
@@ -10,36 +10,8 @@
 
 namespace Gerrie\Component\DataService;
 
-abstract class BaseDataService implements DataServiceInterface
+interface DataServiceInterface
 {
-
-    /**
-     * Name of data service
-     *
-     * @var string
-     */
-    private $name = '';
-
-    /**
-     * API connector like HTTP or SSH
-     *
-     * @var object
-     */
-    private $connector = null;
-
-    /**
-     * Query limit
-     *
-     * @var int|null
-     */
-    private $queryLimit = null;
-
-    /**
-     * Configuration
-     *
-     * @var array
-     */
-    protected $config = array();
 
     /**
      * Sets the name of the data service
@@ -47,20 +19,14 @@ abstract class BaseDataService implements DataServiceInterface
      * @param string $name Name of data service
      * @return void
      */
-    public function setName($name)
-    {
-        $this->name = (string)$name;
-    }
+    public function setName($name);
 
     /**
      * Returns the name of the data service
      *
      * @return string
      */
-    public function getName()
-    {
-        return $this->name;
-    }
+    public function getName();
 
     /**
      * Sets the API connector
@@ -68,20 +34,14 @@ abstract class BaseDataService implements DataServiceInterface
      * @param \Buzz\Browser|\Gerrie\Component\Connection\SSH $connector API connector like HTTP Client
      * @return void
      */
-    public function setConnector($connector)
-    {
-        $this->connector = $connector;
-    }
+    public function setConnector($connector);
 
     /**
      * Returns the API connector
      *
      * @return \stdClass
      */
-    public function getConnector()
-    {
-        return $this->connector;
-    }
+    public function getConnector();
 
     /**
      * Sets the configuration
@@ -89,20 +49,14 @@ abstract class BaseDataService implements DataServiceInterface
      * @param array $config
      * @return void
      */
-    public function setConfig(array $config)
-    {
-        $this->config = $config;
-    }
+    public function setConfig(array $config);
 
     /**
      * Gets the configuration
      *
      * @return array
      */
-    public function getConfig()
-    {
-        return $this->config;
-    }
+    public function getConfig();
 
     /**
      * Sets the query limit
@@ -110,37 +64,21 @@ abstract class BaseDataService implements DataServiceInterface
      * @param int $queryLimit The query limit for Gerrit querys
      * @return void
      */
-    public function setQueryLimit($queryLimit)
-    {
-        $this->queryLimit = intval($queryLimit);
-    }
+    public function setQueryLimit($queryLimit);
 
     /**
      * Gets the query limit
      *
      * @return int
      */
-    public function getQueryLimit()
-    {
-
-        if ($this->queryLimit === null) {
-            $this->setQueryLimit($this->initQueryLimit());
-        }
-
-        return $this->queryLimit;
-    }
+    public function getQueryLimit();
 
     /**
      * Gets the Host
      *
      * @return string
      */
-    public function getHost()
-    {
-        $config = $this->getConfig();
-
-        return $config['host'];
-    }
+    public function getHost();
 
     /**
      * Transforms a JSON string into an array.
@@ -149,14 +87,14 @@ abstract class BaseDataService implements DataServiceInterface
      * @param string $json The json string
      * @return array|null
      */
-    abstract public function transformJsonResponse($json);
+    public function transformJsonResponse($json);
 
     /**
      * Requests projects at the Gerrit server
      *
      * @return array|null
      */
-    abstract public function getProjects();
+    public function getProjects();
 
     /**
      * Requests changesets at the Gerrit server.
@@ -166,7 +104,7 @@ abstract class BaseDataService implements DataServiceInterface
      * @param integer $start Number of changesets which will be skipped
      * @throws \Exception
      */
-    abstract public function getChangesets($projectName, $resumeKey = null, $start = 0);
+    public function getChangesets($projectName, $resumeKey = null, $start = 0);
 
     /**
      * Returns the version of the Gerrit server
@@ -175,5 +113,5 @@ abstract class BaseDataService implements DataServiceInterface
      *
      * @return string
      */
-    abstract public function getVersion();
+    public function getVersion();
 }
