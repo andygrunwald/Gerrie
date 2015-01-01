0a8c9e9d98480b895b9154c9853d29ee89fdb883
Andy Grunwald <andygrunwald@gmail.com>
2015-01-01 19:11:54 +0100

    Introduced TransformerFactory to create Transformers

---
+++ src/Gerrie/Transformer/TransformerFactory.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * This file is part of the Gerrie package.
+ *
+ * (c) Andreas Grunwald <andygrunwald@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Gerrie\Transformer;
+
+class TransformerFactory
+{
+
+    /**
+     * Creates a new (data) transformer
+     *
+     * @param string $entity Entity / name of data to transform (singular)
+     * @param bool $debug
+     * @return TransformerInterface
+     */
+    public static function getTransformer($entity, $debug = false)
+    {
+        $className = 'Gerrie\\Transformer\\' . $entity . 'Transformer';
+        $transformer = new $className();
+
+        if ($debug === true) {
+            $transformer = new DebugTransformDecorator($transformer);
+        }
+
+        return $transformer;
+    }
+}
