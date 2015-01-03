c3785523642ed9d0b8d30a304bea0c3d63401690
Andy Grunwald <andygrunwald@gmail.com>
2015-01-03 23:05:08 +0100

    Added unit test for Configuration default object

--- tests/Gerrie/Tests/Component/Configuration/ConfigurationFactoryTest.php
+++ tests/Gerrie/Tests/Component/Configuration/ConfigurationFactoryTest.php
@@ -42,46 +42,53 @@ class ConfigurationFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('root', $configuration->getConfigurationValue('Database.Username'));
     }
 
-    public function testGetConfigurationByConfigFileAndCommandOptionsAndArguments()
+    protected function getArgvInputExtendedMockObject($isConfigFileOptionSet)
     {
         $mockedMethods = ['isOptionSet', 'getOption', 'getArgument'];
         $argvInputExtended = $this->getMock('Gerrie\Component\Console\ArgvInputExtended', $mockedMethods);
         $argvInputExtended->expects($this->any())
-                          ->method('isOptionSet')
-                          ->withConsecutive(
-                              array($this->equalTo('config-file')),
-                              array($this->equalTo('database-host')),
-                              array($this->equalTo('database-user')),
-                              array($this->equalTo('database-pass')),
-                              array($this->equalTo('database-port')),
-                              array($this->equalTo('database-name'))
-                          )
-                          ->willReturnOnConsecutiveCalls(
-                              $this->returnValue(true),
-                              $this->returnValue(true),
-                              $this->returnValue(true),
-                              $this->returnValue(false),
-                              $this->returnValue(true),
-                              $this->returnValue(false)
-                          );
+            ->method('isOptionSet')
+            ->withConsecutive(
+                array($this->equalTo('config-file')),
+                array($this->equalTo('database-host')),
+                array($this->equalTo('database-user')),
+                array($this->equalTo('database-pass')),
+                array($this->equalTo('database-port')),
+                array($this->equalTo('database-name'))
+            )
+            ->willReturnOnConsecutiveCalls(
+                $this->returnValue($isConfigFileOptionSet),
+                $this->returnValue(true),
+                $this->returnValue(true),
+                $this->returnValue(false),
+                $this->returnValue(true),
+                $this->returnValue(false)
+            );
 
         $argvInputExtended->expects($this->any())
-                          ->method('getOption')
-                          ->withConsecutive(
-                              array($this->equalTo('database-host')),
-                              array($this->equalTo('database-user')),
-                              array($this->equalTo('database-port'))
-                          )
-                          ->willReturnOnConsecutiveCalls(
-                              $this->returnValue('HOST'),
-                              $this->returnValue('USER'),
-                              $this->returnValue('PORT')
-                          );
+            ->method('getOption')
+            ->withConsecutive(
+                array($this->equalTo('database-host')),
+                array($this->equalTo('database-user')),
+                array($this->equalTo('database-port'))
+            )
+            ->willReturnOnConsecutiveCalls(
+                $this->returnValue('HOST'),
+                $this->returnValue('USER'),
+                $this->returnValue('PORT')
+            );
 
         $argvInputExtended->expects($this->any())
-                          ->method('getArgument')
-                          ->with($this->equalTo('instances'))
-                          ->will($this->returnValue(array()));
+            ->method('getArgument')
+            ->with($this->equalTo('instances'))
+            ->will($this->returnValue(array()));
+
+        return $argvInputExtended;
+    }
+
+    public function testGetConfigurationByConfigFileAndCommandOptionsAndArguments()
+    {
+        $argvInputExtended = $this->getArgvInputExtendedMockObject(true);
 
         $configFile = $this->getFixtureConfigFilePath();
         $configuration = ConfigurationFactory::getConfigurationByConfigFileAndCommandOptionsAndArguments($configFile, $argvInputExtended);
@@ -93,5 +100,20 @@ class ConfigurationFactoryTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(null, $configuration->getConfigurationValue('Database.Password'));
         $this->assertEquals('PORT', $configuration->getConfigurationValue('Database.Port'));
         $this->assertEquals('gerrie', $configuration->getConfigurationValue('Database.Name'));
+    }
+
+    public function testGetConfigurationByConfigFileAndCommandOptionsAndArgumentsWithoutConfigFileOption()
+    {
+        $argvInputExtended = $this->getArgvInputExtendedMockObject(false);
+
+        $configFile = $this->getFixtureConfigFilePath();
+        $configuration = ConfigurationFactory::getConfigurationByConfigFileAndCommandOptionsAndArguments($configFile, $argvInputExtended);
+
+        $this->assertInstanceOf('Gerrie\Component\Configuration\Configuration', $configuration);
+
+        $this->assertEquals('HOST', $configuration->getConfigurationValue('Database.Host'));
+        $this->assertEquals('USER', $configuration->getConfigurationValue('Database.Username'));
+        $this->assertEquals(null, $configuration->getConfigurationValue('Database.Password'));
+        $this->assertEquals('PORT', $configuration->getConfigurationValue('Database.Port'));
     }
 }
